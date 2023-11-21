1de6dc542ed631d697d8d69582e5c2efa06acf05
hedonicadapter <filtersome@gmail.com>
2023-11-21 12:29:57 +0100

    hacky transitions

---
+++ wwwroot/BlazorApp2.lib.module.js
@@ -0,0 +1,5 @@
+﻿export function beforeStart(options, extensions) {
+    var customScript = document.createElement('script');
+    customScript.setAttribute('src', '~/JS/TransitionFunctions.js');
+    document.head.appendChild(customScript);
+}
---
+++ wwwroot/JS/TransitionFunctions.js
@@ -0,0 +1,52 @@
+﻿
+navigation.addEventListener("navigate", ()=>{
+    
+})
+function startTransition(){
+    const transitionWrapper = document.querySelector('#transition-wrapper');
+    if (!transitionWrapper) return;
+    
+    transitionWrapper.style.opacity = 1;
+}
+
+function exitTransition(){
+    const transitionWrapper = document.querySelector('#transition-wrapper');
+    if (!transitionWrapper) return;
+    
+    transitionWrapper.style.opacity = 0;
+}
+
+document.body.addEventListener('click', function(e) {
+    if (e.target.tagName.toLowerCase() === 'a') {
+        e.preventDefault()
+        const href = e.target.href
+        
+        const transitionWrapper = document.querySelector('#transition-wrapper');
+        if (!transitionWrapper) {
+            window.location.href = href;
+ 
+        } else {
+            exitTransition();
+            
+            transitionWrapper.addEventListener("transitionend", () => {
+                window.location.href = href
+            })
+        }
+    }
+})
+
+// const observeUrlChange = () => {
+//     let oldHref = document.location.href;
+//     const body = document.querySelector("body");
+//     const observer = new MutationObserver(mutations => {
+//         if (oldHref !== document.location.href) {
+//             oldHref = document.location.href;
+//            
+//             startTransition();
+//         }
+//     });
+//     observer.observe(body, { childList: true, subtree: true });
+// };
+//
+// window.onload = observeUrlChange;
+
